7b7b040feb23b5cb69792e8bc340d9f5db277a94
Lodewyk van der Merwe <lvandermerwe@petsafe.net>
2022-08-19 13:58:03 +0100

    Test sonar run

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 export class TestClass {
 
    public main(): void {
+	const a = "This is a variable";
+	if(a == 'This is a variable') {
+		console.log("code smell using double equals")
+	}
 	try {
 		throw new Error("Should be a code smell");
 	} catch(err) {
