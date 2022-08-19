a54f4aabd58d78fed80d6bfb32b2fc276e1d6722
Lodewyk van der Merwe <lvandermerwe@petsafe.net>
2022-08-19 13:54:39 +0100

    Test sonar run

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 export class TestClass {
 
    public main(): void {
+	try {
+		throw new Error("Should be a code smell");
+	} catch(err) {
+		console.log(err);
+	}
 	console.log("This is a test")
    }
 
